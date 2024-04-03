d24532afdcc83e86ac7d6dda378035b0e89b5583
Krish C Naik <krishnaik06@gmail.com>
2024-04-03 18:45:44 +0530

    Add files via upload

---
+++ api/app.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+from langchain.prompts import ChatPromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langserve import add_routes
+import uvicorn
+import os
+from langchain_community.llms import Ollama
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ['OPENAI_API_KEY']=os.getenv("OPENAI_API_KEY")
+
+app=FastAPI(
+    title="Langchain Server",
+    version="1.0",
+    decsription="A simple API Server"
+
+)
+
+add_routes(
+    app,
+    ChatOpenAI(),
+    path="/openai"
+)
+model=ChatOpenAI()
+##ollama llama2
+llm=Ollama(model="llama2")
+
+prompt1=ChatPromptTemplate.from_template("Write me an essay about {topic} with 100 words")
+prompt2=ChatPromptTemplate.from_template("Write me an poem about {topic} for a 5 years child with 100 words")
+
+add_routes(
+    app,
+    prompt1|model,
+    path="/essay"
+
+
+)
+
+add_routes(
+    app,
+    prompt2|llm,
+    path="/poem"
+
+
+)
+
+
+if __name__=="__main__":
+    uvicorn.run(app,host="localhost",port=8000)
+
---
+++ api/client.py
@@ -0,0 +1,27 @@
+import requests
+import streamlit as st
+
+def get_openai_response(input_text):
+    response=requests.post("http://localhost:8000/essay/invoke",
+    json={'input':{'topic':input_text}})
+
+    return response.json()['output']['content']
+
+def get_ollama_response(input_text):
+    response=requests.post(
+    "http://localhost:8000/poem/invoke",
+    json={'input':{'topic':input_text}})
+
+    return response.json()['output']
+
+    ## streamlit framework
+
+st.title('Langchain Demo With LLAMA2 API')
+input_text=st.text_input("Write an essay on")
+input_text1=st.text_input("Write a poem on")
+
+if input_text:
+    st.write(get_openai_response(input_text))
+
+if input_text1:
+    st.write(get_ollama_response(input_text1))
